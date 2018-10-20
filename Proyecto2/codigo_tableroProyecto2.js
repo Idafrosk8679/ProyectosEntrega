b4fd3399e40260fac20defe3f1b00a8661de36c0
Idafrosk8679 <aitor.1999@hotmail.com>
2018-10-20 18:50:58 +0200

    Comprobando el tablero. Funcion

--- Proyecto2/codigo_tableroProyecto2.js
+++ Proyecto2/codigo_tableroProyecto2.js
@@ -2,6 +2,7 @@ var f = Number(prompt('Introduce numero de FILA'));
 var c = Number(prompt('Introduce numero de COLUMNA'));
 var tablero=CalcularTablero(f,c)
 
+
 function CalcularTablero(f,c) {
   if  (f >= 1 && f <= 6){
     console.log('True');
