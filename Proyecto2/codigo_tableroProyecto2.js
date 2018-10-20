cb9691cfdefaff0c1d9ae6d37b2e80a768772506
Idafrosk8679 <aitor.1999@hotmail.com>
2018-10-20 18:45:31 +0200

    Comprobando el tablero

---
+++ Proyecto2/codigo_tableroProyecto2.js
@@ -0,0 +1,15 @@
+var f = Number(prompt('Introduce numero de FILA'));
+var c = Number(prompt('Introduce numero de COLUMNA'));
+var tablero=CalcularTablero(f,c)
+
+function CalcularTablero(f,c) {
+  if  (f >= 1 && f <= 6){
+    console.log('True');
+  }else{
+    console.log('False');
+  }if (c >= 1 && c <= 6){
+    console.log('True');
+  }else{
+    console.log('False');
+  }
+}
