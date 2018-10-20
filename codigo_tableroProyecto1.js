731ec15f288ccfa40c647e50372692e42ea74ad6
Idafrosk8679 <aitor.1999@hotmail.com>
2018-10-20 18:37:29 +0200

    Comprobando el tablero

--- codigo_tableroProyecto1.js
+++ codigo_tableroProyecto1.js
@@ -2,9 +2,11 @@ var f = Number(prompt('Introduce numero de FILA'));
 var c = Number(prompt('Introduce numero de COLUMNA'));
 
   if  (f >= 1 || f <= 6){
+    console.log('Datos Acceptados');
   }else{
     alert('Introduza un numero de fila dentro del rango');
   }if (c >= 1 && c <= 6){
+    console.log('Datos Acceptados');
   }else{
     alert('Introduza un numero de columna dentro del rango');
 }
